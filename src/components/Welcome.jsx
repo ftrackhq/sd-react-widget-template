a798f2113b0c1c964e62afe6c3d949304b1e9ee1
lorenzo angeli <hdd@users.noreply.github.com>
2023-02-01 16:55:05 +0100

    rework welcome page

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -14,14 +14,18 @@ function Welcome(props) {
     return (
         <div className="welcome">
             <img className="logo" src={logo} />
-            <h2 onClick={() => onEntityClick(selection)}> {selection.name}[{selection.__entity_type__}] with id {selection.id}</h2>
             <h3>Theme : {theme}</h3>
+            <hr/>
+            <button onClick={() => onEntityClick(selection)} >{selection.name}[{selection.__entity_type__}] with id {selection.id}</button>
             <hr/>
             <h3>Instance : {session.serverUrl}</h3>
             <h3>Server Version : {session.serverVersion }</h3>
+            <h3>Server Storage: {session.serverInformation.storage_scenario.data.location_name}</h3>
             <h3>User : {session.apiUser}</h3>
             <hr/>
-            <h3>BASE_URL : {import.meta.env.BASE_URL}</h3>
+            <h3>Application base url : {import.meta.env.BASE_URL}</h3>
+            <h3>Application mode : {import.meta.env.MODE}</h3>
+
         </div>
     )
 }
