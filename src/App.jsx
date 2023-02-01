4fdf3f9650aab49ee3e1f1eedd3821e2a642b09f
lorenzo angeli <hdd@users.noreply.github.com>
2023-02-01 11:49:24 +0100

    Update with custom Welcome debug component

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,17 @@ import { getEntity , getActiveTheme } from "@ftrack/web-widget";
 import useSession from "./session_context";
 import {SETTINGS} from './config'
 import logo from './assets/ftrack-studio-logo-dark.png';
+import { ThreeCircles } from  'react-loader-spinner'
 
 import './App.css'
-
+import Welcome from './components/Welcome.jsx'
 
 function loadStyleSheet(theme){
     var head = document.getElementsByTagName('head')[0];
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = process.env.PUBLIC_URL+ '/style-' + theme + '.css';
+    link.href = '/style-' + theme + '.css';
     console.log(link)
     head.appendChild(link);
 }
@@ -26,6 +27,7 @@ function App() {
   loadStyleSheet(theme)
 
   const [selection, setSelection] = useState(current_entity);
+  const [isLoading, setIsLoading] = useState(false);
 
 
   function onFtrackWidgetUpdate(ftrack_event){
@@ -34,13 +36,31 @@ function App() {
   }
 
   useEffect(()=>{
+      setIsLoading(true)
       window.addEventListener('ftrackWidgetUpdate', onFtrackWidgetUpdate);
-  }, [session])
+      setIsLoading(false)
+  }, [session, selection])
 
+    const render_widget = (
+        <div>
+            <Welcome session={session} theme={theme} selection={selection} logo={logo}/>
+        </div>
+    )
 
     return (
         <div>
-            <img src={logo} className="App-logo" alt="logo" />
+          {isLoading ? <ThreeCircles
+              height="250"
+              width="250"
+              color="#935ba2"
+              wrapperStyle={{}}
+              wrapperClass="spinner"
+              visible={true}
+              ariaLabel="three-circles-rotating"
+              outerCircleColor=""
+              innerCircleColor=""
+              middleCircleColor=""
+            /> : render_widget}
         </div>
     )
 }
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,23 @@
+import {useEffect, useState} from "react";
+
+function Welcome(props) {
+    const session = props.session
+    const theme = props.theme
+    const selection = props.selection
+    const logo = props.logo
+
+    return (
+        <div>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h3>Instance : {session.serverUrl}</h3>
+            <h3>User :  {session.apiUser}</h3>
+            <h3>Theme : {theme}</h3>
+            <h3>Selection : {selection.type} : {selection.id} </h3>
+            <hr/>
+            <h3>BASE_URL : {import.meta.env.BASE_URL}</h3>
+
+        </div>
+    )
+}
+
+export default Welcome;
