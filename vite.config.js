5bc7e125eecdce86206d25c084897353680bea60
lorenzo angeli <hdd@users.noreply.github.com>
2023-02-06 15:24:24 +0100

    update deployment path

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server:{
-    port: 3000
-  },
-  base: ''
+    port: 3000,
+    base:'./'
+  }
 })
